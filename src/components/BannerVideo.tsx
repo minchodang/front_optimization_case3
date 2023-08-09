28e466e1a93eefe870a05c220449283eae86b6f9
minsuKang <minsu910725@gmail.com>
2023-08-09 09:39:01 +0900

    Fix: 비디오 및 font 최적화

--- src/components/BannerVideo.tsx
+++ src/components/BannerVideo.tsx
@@ -1,18 +1,41 @@
+import FontFaceObserver from 'fontfaceobserver';
+import { useEffect, useMemo, useState } from 'react';
 import video from '../assets/banner-video.mp4';
+import video_webm from '../assets/banner-video.webm';
 
 export const BannerVideo = () => {
+  const [isFontLoaded, setIsFontLoaded] = useState(false);
+  const font = useMemo(() => {
+    return new FontFaceObserver('BMYEONSUNG');
+  }, []);
+
+  useEffect(() => {
+    font.load(null, 20000).then(function () {
+      console.log('font has loaded');
+      setIsFontLoaded(true);
+    });
+  }, [font]);
+
   return (
     <div className="BannerVideo w-full h-screen overflow-hidden relative bg-texture">
       <div className="absolute h-screen w-full left-1/2">
         <video
-          src={video}
           className="absolute translateX--1/2 h-screen max-w-none min-w-screen -z-1 bg-black min-w-full min-h-screen"
           autoPlay
           loop
           muted
-        />
+        >
+          <source src={video_webm} type="video/webm" />
+          <source src={video} type="video/mp4" />
+        </video>
       </div>
-      <div className="w-full h-full flex justify-center items-center">
+      <div
+        className="w-full h-full flex justify-center items-center"
+        style={{
+          opacity: isFontLoaded ? 1 : 0,
+          transition: 'opacity 0.3s ease',
+        }}
+      >
         <div className="text-white text-center">
           <div className="text-6xl leading-none font-semibold">KEEP</div>
           <div className="text-6xl leading-none font-semibold">CALM</div>
--- src/react-app-env.ts
+++ src/react-app-env.ts
@@ -4,3 +4,4 @@ declare module '*.jpeg';
 declare module '*.jpg';
 declare module '*.mp4';
 declare module '*.webp';
+declare module '*.webm';
